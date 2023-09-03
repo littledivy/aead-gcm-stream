8649645793c653a3c0550cce5b1bc780996ed395
Divy Srivastava <dj.srivastava23@gmail.com>
2023-09-03 11:52:32 +0530

    Counter refactor

--- src/lib.rs
+++ src/lib.rs
@@ -2,19 +2,19 @@ use aead::{consts::U16, generic_array::GenericArray, Key, KeyInit, KeySizeUser};
 use cipher::{BlockCipher, BlockEncrypt, BlockSizeUser};
 use ghash::{universal_hash::UniversalHash, GHash};
 
+mod ctr;
+
+use ctr::Ctr;
+
 pub struct AesGcm<Aes>
 where
     Aes: BlockCipher + BlockSizeUser<BlockSize = U16> + BlockEncrypt,
 {
     /// Encryption cipher
-    cipher: Aes,
+    ctr: Ctr<Aes>,
 
     /// GHASH authenticator
     ghash: GHash,
-
-    /// Counter mode with a 32-bit big endian counter.
-    j0: GenericArray<u8, U16>,
-    j0_ct: GenericArray<u8, U16>,
 
     len: usize,
 
@@ -38,14 +38,10 @@ where
         cipher.encrypt_block(&mut ghash_key);
 
         let ghash = GHash::new(&ghash_key);
-        let j0 = GenericArray::from([0u8; 16]);
-        let j0_ct = j0;
-
+        let ctr = Ctr::new(cipher);
         Self {
-            cipher,
+            ctr,
             ghash,
-            j0,
-            j0_ct,
             len: 0,
             aad_len: 0,
         }
@@ -62,17 +58,13 @@ where
     }
 
     pub fn init(&mut self, nonce: &[u8; 12]) {
-        self.j0[..12].copy_from_slice(nonce);
-        self.j0[15] = 1;
-
-        self.j0_ct = self.j0.clone();
-        self.cipher.encrypt_block(&mut self.j0_ct);
+        self.ctr.init(nonce);
     }
 
     pub fn encrypt(&mut self, block: &mut [u8]) {
         let mut blocks = block.chunks_exact_mut(16);
         for block in &mut blocks {
-            self.encrypt_block_inner(block.into());
+            self.ctr.encrypt_block_inner(block.into());
         }
 
         let tail = blocks.into_remainder();
@@ -80,21 +72,13 @@ where
         if !tail.is_empty() {
             let mut padded_block = [0; 16];
             padded_block[..tail.len()].copy_from_slice(tail);
-            self.encrypt_block_inner(&mut padded_block);
+            self.ctr.encrypt_block_inner(&mut padded_block);
 
             tail.copy_from_slice(&padded_block[..tail.len()]);
         }
 
         self.ghash.update_padded(block);
         self.len += block.len()
-    }
-
-    fn encrypt_block_inner(&mut self, block: &mut [u8]) {
-        self.inc();
-        let mut ek = self.j0;
-        self.cipher.encrypt_block(&mut ek);
-
-        block.iter_mut().zip(ek).for_each(|(a, b)| *a ^= b);
     }
 
     pub fn decrypt(&mut self, block: &mut [u8]) {
@@ -102,7 +86,7 @@ where
 
         let mut blocks = block.chunks_exact_mut(16);
         for block in &mut blocks {
-            self.encrypt_block_inner(block.into());
+            self.ctr.encrypt_block_inner(block.into());
         }
 
         let tail = blocks.into_remainder();
@@ -110,19 +94,12 @@ where
         if !tail.is_empty() {
             let mut padded_block = [0; 16];
             padded_block[..tail.len()].copy_from_slice(tail);
-            self.encrypt_block_inner(&mut padded_block);
+            self.ctr.encrypt_block_inner(&mut padded_block);
 
             tail.copy_from_slice(&padded_block[..tail.len()]);
         }
 
         self.len += block.len()
-    }
-
-    fn inc(&mut self) {
-        self.j0[15] = self.j0[15].wrapping_add(1);
-        if self.j0[15] == 0 {
-            self.j0[14] = self.j0[14].checked_add(1).unwrap();
-        }
     }
 
     pub fn finish(mut self) -> GenericArray<u8, U16> {
@@ -137,7 +114,7 @@ where
         let mut tag = self.ghash.finalize();
         tag.as_mut_slice()
             .iter_mut()
-            .zip(self.j0_ct)
+            .zip(self.ctr.j0_ct)
             .for_each(|(a, b)| *a ^= b);
 
         tag
